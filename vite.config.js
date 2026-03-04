d5985c5c337c3939934c6ea0cd6d3b704f857ee5
Luis Claudio Silveira <luiscrsilveira@gmail.com>
2026-03-03 23:12:54 -0500

    chore: setup gh-pages and base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/elite-floors/',
     plugins: [
         react(),
         tailwindcss(),
